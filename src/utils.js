206825c81a608be5971e15ab797a86e1e432652c
bcameron1489 <bcameron1489@gmail.com>
2019-06-30 05:06:34 -0400

    initial commit

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+console.log('utils.js is running!!!')
+
+
+export const square = (x) => x * x
+
+export const add = (a, b) => a + b
+
+const subtract = (a , b) => a - b
+
+export default subtract
+
+// exports - default export - named exports
+
+
+// named exports
+// export { square, add }
+
+
+//default exports
