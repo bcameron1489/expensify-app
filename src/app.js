ad89326240f82ed94a1138a6d5e118bd13c32605
bcameron1489 <bcameron1489@gmail.com>
2019-06-30 05:32:01 -0400

    Setup react router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 // install -> import -> use
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/style.scss'
 
-ReactDOM.render(<p>This is a boilerplate</p>, document.getElementById('app'))
+const ExpenseDashboardPage = () => (
+  <div>
+    this is from my dashboard component
+  </div>
+)
+
+const AddExpensePage = () => (
+  <div>
+    this is from my add expense component
+  </div>
+)
+
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage}/>
+      <Route path="/create" component={AddExpensePage} />
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
