6eda485bb95c4dc525d997842d3cc918a496ddb5
bcameron1489 <bcameron1489@gmail.com>
2019-06-30 19:03:23 -0400

    Setup dynamic route for edit
    Remove edit link from header

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const EditExpensePage = () => (
-  <div>
-    this is from my edit expense component
-  </div>
-)
+const EditExpensePage = (props) => {
+  return (
+    <div>
+      Editing the expense with id of {props.match.params.id}
+    </div>
+  )
+}
+
+
 
 export default EditExpensePage
